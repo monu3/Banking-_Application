f2be0a6e7668d4a819174460846bb0848d65afcf
monu3 <m9800564411@gmail.com>
2024-06-15 00:22:20 +0545

    new java project

---
+++ src/main/java/org/banking/banking_application/Entity/Accounts.java
@@ -0,0 +1,29 @@
+package org.banking.banking_application.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+public class Accounts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id ;
+
+    @Column(name = "account_holder", nullable = false, length = 100)
+    public String accountHolderName;
+
+    @Column(name = "account_number", nullable = true, length = 20)
+    public String accountNumber;
+
+    @Column(name = "Balance", nullable = false, length = 100)
+    public double balance;
+}
